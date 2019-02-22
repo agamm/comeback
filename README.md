a3eec4ca7c702833e5fb10dca45d4dd1c29854f2
agam360 <agam360@gmail.com>
2019-02-22 22:48:07 +0200

    Initial commit, add gitignore, pipenv, basic setup and cli usage

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+
+### How to run locally
+`pipenv shell`
+`pipenv install -e .`
+`comeback --help`
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='comeback',
+    version='0.0.1',
+    py_modules=['src'],
+    license="MIT",
+    install_requires=[
+        'Click',
+    ],
+    entry_points={
+        'console_scripts': ['comeback=src.main:cli'],
+    },
+)
