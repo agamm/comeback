28384489ae25983d9b4c683a151043b803573cc0
agam360 <agam360@gmail.com>
2019-03-02 18:09:41 +0200

    Add returns to linux and mac

--- comeback/plugins/pycharm/main.py
+++ comeback/plugins/pycharm/main.py
@@ -14,36 +14,36 @@ def run_windows(cwd):
     pycharm_settings_dir = list(home.glob('*pycharm*'))
 
     if not pycharm_settings_dir or len(pycharm_settings_dir) == 0:
-        return False, "pycharm's settings dir was not found."
+        return False, 'pycharm\'s settings dir was not found.''
 
     pycharm_settings_dir = pycharm_settings_dir[0]
     # I really hate how JetBrains are forcing me to puke
     home_settings_file = pycharm_settings_dir.joinpath("system", ".home")
     if not home_settings_file.exists():
-        return False, "pycharm's settings .home file was not found."
+        return False, 'pycharm\'s settings .home file was not found.'
 
     install_path = utils.read_file(home_settings_file)
     pycharm_install_path = pathlib.Path(install_path)
     if not pycharm_install_path.exists():
-        return False, f"pycharm's install path from the settings \
-            file is not correct, {utils.report_issue()}"
+        return False, f'pycharm\'s install path from the settings \
+            file is not correct, {utils.report_issue()}'
 
     # I actually checked and the pycharm.exe is 32bit,
     #  let's only use it if the 64 is not found
-    pycharm_exe32 = pycharm_install_path.joinpath("bin", "pycharm.exe")
-    pycharm_exe64 = pycharm_install_path.joinpath("bin", "pycharm64.exe")
+    pycharm_exe32 = pycharm_install_path.joinpath('bin', 'pycharm.exe')
+    pycharm_exe64 = pycharm_install_path.joinpath('bin', 'pycharm64.exe')
 
     pyc32 = pycharm_exe32.exists()
     pyc64 = pycharm_exe64.exists()
 
     if not pyc32 and pyc64:
-        return False, "pycharm's exe files are not found."
+        return False, 'pycharm\'s exe files are not found.'
     elif pyc64:
         utils.run(f'{pycharm_exe64} {cwd}')
     else:
         utils.run(f'{pycharm_exe64} {cwd}')
 
-    return True, "Found pycharm"
+    return True, 'Found pycharm'
 
 
 def run_linux(cwd, pycharm_path):
@@ -52,10 +52,10 @@ def run_linux(cwd, pycharm_path):
             return False, \
                    'PyCharm not found, please provide pycharm_path option'
         utils.run(f'{pycharm_path} {cwd}')
-        return True, "Found pycharm"
+        return True, 'Found pycharm'
 
     utils.run(['pycharm-community', cwd], no_shell=True)
-    return True, "Found pycharm"
+    return True, 'Found pycharm'
 
 
 def run_mac(cwd):
@@ -79,6 +79,6 @@ def run_plugin(cwd, pycharm_path=False):
     if platform == "windows":
         return run_windows(cwd)
     elif platform == "linux":
-        run_linux(cwd, pycharm_path)
+        return run_linux(cwd, pycharm_path)
     elif platform == "mac":
-        run_mac(cwd)
+        return run_mac(cwd)
