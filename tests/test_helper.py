eefafee29b3ecb19373506c4f66583f4b3fe241f
agam360 <agam360@gmail.com>
2019-03-23 00:55:28 +0200

    Double to single quotes #2

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -7,7 +7,7 @@ def create_directory(tmp_path, dirname=TEST_DIR):
     return test_dir
 
 
-def create_test_file(tmp_path, text_content, file_name="test.txt"):
+def create_test_file(tmp_path, text_content, file_name='test.txt'):
     test_dir = create_directory(tmp_path)
     test_file = test_dir / file_name
     test_file.write_text(text_content)
@@ -19,7 +19,7 @@ def create_test_dir(tmp_path, empty=False):
     if empty:
         return test_dir
 
-    create_directory(tmp_path, "1")
-    create_directory(tmp_path, "2")
-    create_directory(tmp_path, "3")
+    create_directory(tmp_path, '1')
+    create_directory(tmp_path, '2')
+    create_directory(tmp_path, '3')
     return tmp_path / TEST_DIR
